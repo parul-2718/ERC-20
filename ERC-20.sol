da12b68d09c3ce97910628f614973e299fef4011
Yadav <parul.g.yadav@accenture.com>
2023-07-25 15:29:38 +0530

    Your commit message here

--- ERC-20.sol
+++ ERC-20.sol
@@ -50,3 +50,7 @@ mapping(address => mapping (address => uint256)) allowed;
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
+
+
+function BalanceOfAt(address _owner) public view returns (uint){
+}
